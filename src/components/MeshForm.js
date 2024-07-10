27655a4d2fd8896a00614d5b0ecfc47544817428
Chris Gentibano <christopher.gentibano@meshconnect.com>
2024-07-10 14:37:00 -0700

    removed config

--- src/components/MeshForm.js
+++ src/components/MeshForm.js
@@ -1,42 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './MeshForm.css';
 
 const MeshForm = () => {
   const [selectedBroker, setSelectedBroker] = useState("Mesh Catalog");
   const [buttonVisible, setButtonVisible] = useState(true);
-  const [popupWindow, setPopupWindow] = useState(null);
 
-  const baseUrl = process.env.REACT_APP_PORTAL_BASE_URL; // Define your URL in a .env file in the src directory
-
+  const baseUrl = process.env.REACT_APP_PORTAL_BASE_URL; //define your URL in a .env file in the src directory
+  
   const handleBrokerChange = (event) => {
     setSelectedBroker(event.target.value);
     setButtonVisible(event.target.value !== '');
   };
 
   const handleOpenPopupClick = () => {
-    const url = selectedBroker === "Mesh Catalog"
-      ? `${baseUrl}/meshPopup`
-      : `${baseUrl}/meshPopup?broker=${selectedBroker}`;
-    const newPopupWindow = window.open(url, 'MeshLinkPopup', 'width=400,height=600');
-
-    if (newPopupWindow) {
-      setPopupWindow(newPopupWindow);
+    if (selectedBroker === "Mesh Catalog"){
+      window.open(`${baseUrl}/meshPopup`, 'MeshLinkPopup', 'width=400,height=600');
+    }
+    else {
+      window.open(`${baseUrl}/meshPopup?broker=${selectedBroker}`, 'MeshLinkPopup', 'width=400,height=600');
+      popupWindow.document.title = 'Mesh Popup Window';
+      popupWindow.document.body.style.backgroundColor = '#f0f0f0';
     }
   };
-
-  useEffect(() => {
-    const checkPopupClosed = () => {
-      if (popupWindow && popupWindow.closed) {
-        setPopupWindow(null);
-        // Perform any necessary cleanup or state updates here
-        console.log("Popup window closed");
-      }
-    };
-
-    const intervalId = setInterval(checkPopupClosed, 1000);
-
-    return () => clearInterval(intervalId);
-  }, [popupWindow]);
 
   return (
     <div className="mesh-form-container">
